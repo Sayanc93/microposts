d84906574995a68e68d12f055d10a4c70381cc39
Sayan <mail.sayanc@gmail.com>
2015-06-08 03:28:17 +0530

    static-pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,16 +5,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Sample Twitter"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Sample Twitter"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Sample Twitter"
   end
 
 end
