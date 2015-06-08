6778545645cb4e6c0e36512dea70bce395d78d8b
Sayan <mail.sayanc@gmail.com>
2015-06-09 02:47:38 +0530

    Adding bootstrap and integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get 'contact' => 'static_pages#contact'
+
+  get 'sign_up' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
