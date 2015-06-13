861ada4bdb9a4f01bf6afdb8b364ad03daf900b1
Sayan <mail.sayanc@gmail.com>
2015-06-14 00:35:09 +0530

    final heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,4 +26,12 @@
 	  content = Faker::Lorem.sentence(5)
 	  users.each { |user| user.microposts.create!(content: content) }
 	end
+	
+	users = User.all
+	user  = users.first
+	following = users[2..50]
+	followers = users[3..40]
+	following.each { |followed| user.follow(followed) }
+	followers.each { |follower| follower.follow(user) }
+
 
