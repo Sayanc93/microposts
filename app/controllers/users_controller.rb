e4cdcea1e578c239d9572c47faf6e499f66ff052
Sayan <mail.sayanc@gmail.com>
2015-06-15 01:01:53 +0530

    home page css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   	if @user.save
       log_in @user
   		flash[:success] = "Welcome to the Sample Twitter!"
-  		redirect_to @user
+  		redirect_to root_path
   	else
   		render 'new'
   	end
